3133b73efb16004e8b69d41e3af076c1703786f6
David Sibley <dsibley@Davids-iMac.attlocal.net>
2022-07-26 18:17:59 -0400

    adding private ip database option for cloud run and setting other options

--- fast_project/app/database.py
+++ fast_project/app/database.py
@@ -1,4 +1,5 @@
 import os
+import ssl
 
 import pg8000
 import sqlalchemy
@@ -9,13 +10,15 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 #Sqlite config
-# SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-# # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+def get_sqlite_engine():
+    SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+    # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
-# engine = create_engine(
-#     #check_same_thread = False only for sqlite
-#     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-# )
+    sqlite_engine = create_engine(
+        #check_same_thread = False only for sqlite
+        SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    )
+    return sqlite_engine
 
 #Postgres Config
 # user = os.environ.get('POSTGRES_DB_USER')
@@ -31,6 +34,48 @@ from sqlalchemy.orm import sessionmaker
 
 # Base = declarative_base() 
 
+# Cloud Private IP Connector - for use inside GCP as in Cloud Run instance
+# Reference:
+# https://cloud.google.com/sql/docs/postgres/connect-run?hl=en#private-ip
+# https://github.com/GoogleCloudPlatform/python-docs-samples/blob/HEAD/cloud-sql/postgres/sqlalchemy/connect_tcp.py
+
+
+# connect_tcp_socket initializes a TCP connection pool
+# for a Cloud SQL instance of Postgres.
+def connect_tcp_socket() -> sqlalchemy.engine.base.Engine:
+    # Note: Saving credentials in environment variables is convenient, but not
+    # secure - consider a more secure solution such as
+    # Cloud Secret Manager (https://cloud.google.com/secret-manager) to help
+    # keep secrets safe.
+    db_host = os.environ["POSTGRES_HOST"]  # e.g. '127.0.0.1' ('172.17.0.1' if deployed to GAE Flex)
+    db_user = os.environ["POSTGRES_USER"]  # e.g. 'my-db-user'
+    db_pass = os.environ["POSTGRES_PASS"]  # e.g. 'my-db-password'
+    db_name = os.environ["POSTGRES_DB"]  # e.g. 'my-database'
+    db_port = os.environ["POSTGRES_PORT"]  # e.g. 5432
+
+    connect_args = {}
+    pool = sqlalchemy.create_engine(
+        # Equivalent URL:
+        # postgresql+pg8000://<db_user>:<db_pass>@<db_host>:<db_port>/<db_name>
+        sqlalchemy.engine.url.URL.create(
+            drivername="postgresql+pg8000",
+            username=db_user,
+            password=db_pass,
+            host=db_host,
+            port=db_port,
+            database=db_name,
+        ),
+        connect_args=connect_args,
+        # ...
+    )
+    return pool
+
+#Cloud SQL Connector - for running locally but connecting to Cloud SQL instance
+# Reference: 
+# https://cloud.google.com/sql/docs/postgres/connect-connectors#python_1
+# https://github.com/GoogleCloudPlatform/cloud-sql-python-connector/blob/HEAD/tests/system/test_pg8000_connection.py
+
+# Used with GOOGLE_APPLICATION_CREDENTIALS pointing to key json file
 
 # The Cloud SQL Python Connector can be used along with SQLAlchemy using the
 # 'creator' argument to 'create_engine'
@@ -55,7 +100,14 @@ def init_connection_engine() -> sqlalchemy.engine.Engine:
     pool.dialect.description_encoding = None
     return pool
 
-engine = init_connection_engine()
+db_connection_option=os.environ['DB_CONNECTION_OPTION']
+
+if db_connection_option == 'SQLITE':
+    engine = get_sqlite_engine()
+elif db_connection_option == 'CLOUD_SQL_CONNECTOR':
+    engine = init_connection_engine()
+elif db_connection_option == 'PRIVATE_IP':
+    engine = connect_tcp_socket()
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
