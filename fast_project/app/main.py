9d6405d84536a8e21afaa481c2342089bc0d2a39
David Sibley <dsibley@Davids-iMac.attlocal.net>
2022-07-28 22:24:02 -0400

    final touches on inital phase of oauth2 with password auth

--- fast_project/app/main.py
+++ fast_project/app/main.py
@@ -181,6 +181,7 @@ class NetflixTitleFilter(Filter):
 # End points 
 @app.post("/users/", response_model=schemas.UserResponse)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)):
+#def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_email(db, email=user.email)
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
