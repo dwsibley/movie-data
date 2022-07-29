13a03d008bf210b7479230a4f08cb29458f74454
David Sibley <dsibley@Davids-iMac.attlocal.net>
2022-07-28 20:47:54 -0400

    initial OAuth2 with password, bearer and JWT

--- fast_project/app/crud.py
+++ fast_project/app/crud.py
@@ -4,26 +4,37 @@ from datetime import datetime
 from sqlalchemy import select, or_
 from sqlalchemy.orm import Session
 
+from passlib.context import CryptContext
+
 from . import models, schemas
 
 # import models, schemas
 
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
 
 def get_user(db: Session, user_id: int):
     return db.query(models.User).filter(models.User.id == user_id).first()
 
-
 def get_user_by_email(db: Session, email: str):
     return db.query(models.User).filter(models.User.email == email).first()
 
+def get_user_by_username(db: Session, username: str):
+    return db.query(models.User).filter(models.User.username == username).first()
 
 def get_users(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.User).offset(skip).limit(limit).all()
 
 
 def create_user(db: Session, user: schemas.UserCreate):
-    hashed_password = hashlib.sha256(user.password.encode()).hexdigest()
-    db_user = models.User(email=user.email, password=hashed_password)
+    #hashed_password = hashlib.sha256(user.password.encode()).hexdigest()
+    hashed_password = get_password_hash(user.password)
+    db_user = models.User(
+        username=user.username,
+        email=user.email,
+        password=hashed_password)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
--- fast_project/app/schemas.py
+++ fast_project/app/schemas.py
@@ -21,18 +21,32 @@ from pydantic import BaseModel
 
 
 class UserBase(BaseModel):
+    username: str
     email: str
 
 class UserCreate(UserBase):
     password: str
 
-class User(UserBase):
-    id: int
+class UserFullDb(UserBase):
+    password: str
+
+class UserResponse(UserBase):
+    #id: int
     is_active: bool
     #items: list[Item] = []
+    username: str
+    email: str | None = None
 
     class Config:
         orm_mode = True
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: str | None = None
 
 class NetflixNameBase(BaseModel):
     name: str
